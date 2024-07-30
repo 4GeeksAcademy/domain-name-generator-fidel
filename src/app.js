6f58737e17ac2b3be6a662bfd85f54b11bf6d8e0
Fidel  Nieto <nietofidel25@gmail.com>
2024-07-30 00:19:14 +0000

    upgrading names - some uppercases, changeing names of domainNames variables

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
-const pronoun = ["the", "our", "my", "your"];
-const adj = ["great", "big", "tremendous", "trusty"];
-const noun = ["jogger", "racoon", "dog"];
-const domain = [".com", ".es", ".us"];
 
-let domainName = "";
-let allDomain = [];
+// Big arrays, works too
+// const pronoun = ["the", "our", "my", "your"];
+// const adj = ["great", "big", "tremendous", "trusty"];
+// const noun = ["jogger", "racoon", "dog"];
+// const domain = [".com", ".es", ".us"];
+
+// Small array, just to see all the items on console :)
+const pronoun = ["The", "My", "Your"];
+const adj = ["tremendous", "trusty"];
+const noun = ["racoon", "dog"];
+const domain = [".com", ".es"];
+
+let nameSelected = "";
+let domainName = [];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < domain.length; l++) {
-        domainName = `${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`;
-        allDomain.push(domainName);
+       
+        nameSelected = `${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`;
+        domainName.push(nameSelected);
       }
     }
   }
 }
-console.log(allDomain); // eslint-disable-line no-console
+console.log(domainName); // eslint-disable-line no-console
